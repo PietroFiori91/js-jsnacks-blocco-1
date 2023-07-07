0010e5d9683a74b3320a31b9dd450b7fd67fdefa
PietroFiori91 <pederflowers@hotmail.it>
2023-07-07 10:51:08 +0200

    somma

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,7 +1,18 @@
-// const numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let somma = 0;
 
-// for (let i = 0; i < numeri.length; i += 2) {
-//   console.log(numeri[i]);
+for (let i = 0; i < numeri.length; i += 2) {
+  console.log(numeri[i]);
+  somma += numeri[i];
+  console.log(somma);
+}
+
+// const somma = numeri.length.value;
+// console.log(somma);
+// BRUTTO TENTATIVO
+
+// const listaNumeri = [];
+
+// for (let i = o; i < 30; i++) {
+//   const numero = Math.floor(Math.random() * 100) + 1;
 // }
-
-console.log(ciao);
